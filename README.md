47931c23c6bf2628b8d1b24e89b1a3667637d4ce
wuchong <2374175164@qq.com>
2019-07-22 09:09:40 +0800

    first commit

---
+++ README.md
@@ -0,0 +1,5 @@
+## 初始化一个react-typescript项目
+- 新建tsconfig.json文件，tsconfig.json是用来配置typescript项目设置，放在项目根目录，可以通过`tsc --init`去生成。
+- 安装react相关的类型定义文件，`npm i -s @types/react @types/react-dom`
+- 编写`*.tsx`文件
+- 编写`webpack.config.js`文件
---
+++ webpack.config.js
@@ -0,0 +1,26 @@
+const { CleanWebpackPlugin } = require('clean-webpack-plugin');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+module.exports = {
+  entry: './main.tsx',
+  plugins: [
+    new CleanWebpackPlugin({
+      cleanAfterEveryBuildPatterns: ['public/build']
+    }),
+    new HtmlWebpackPlugin({
+      template: 'index.html'
+    }),
+  ],
+  output: {
+    path: __dirname + '/public',
+    filename: 'build/[name].[contenthash].js'
+  },
+  resolve: {
+    extensions: ['.ts', '.tsx', '.js']
+  },
+  module: {
+    rules: [
+      { test: /\.tsx?$/, loader: 'ts-loader' }
+    ]
+  }
+}
